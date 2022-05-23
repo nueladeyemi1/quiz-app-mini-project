47255de425559934f7750e8d86eab072ee450744
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2022-05-23 14:48:28 +0100

    first commit

---
+++ questions/question.js
@@ -0,0 +1,58 @@
+//Creating an array and passing the number, questions, options and answers.
+let questions = [
+    {
+        numb : 1,
+        question : 'Which is the largest Island in the World?',
+        answer : 'Green Island',
+        options : [
+            'Long Island',
+            'Green Island',
+            'Santanel Island',
+            'Great Beach Island'
+        ]
+    },
+    {   
+    numb : 2,
+        question : 'What is the world\'s most populated country?',
+        answer : 'China',
+        options : [
+            'USA',
+            'Russia',
+            'China',
+            'India'
+        ]
+    },
+    {
+        numb : 3,
+        question : 'What is the capital of the Philippines?',
+        answer : 'Manilla',
+        options : [
+            'Manilla',
+            'Dili',
+            'Marawi',
+            'Jakarta'
+        ]
+    },
+    {
+        numb : 4,
+        question : 'The Great Barrier Reef is off the coast of which country',
+        answer : 'Australia',
+        options : [
+            'Australia',
+            'Fiji',
+            'South Africa',
+            'New Zealand'
+        ]
+    },
+    {
+        numb : 5,
+        question : 'What is the World\'s Smallest Country?',
+        answer : 'Vatican City',
+        options : [
+            'Vatican City',
+            'Isreal',
+            'Monaco',
+            'Greece'
+        ],
+    }
+];
